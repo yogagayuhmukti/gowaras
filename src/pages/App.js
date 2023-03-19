853c9c1898a078a20ba3b939c43d0281537838df
gm906~ <gayuhmukti906@gmail.com>
2023-03-19 14:54:55 +0700

    connect whatsapp

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { Rute } from "../config";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return <Rute />;
--- src/pages/Pendaftaran/Pendaftaran.jsx
+++ src/pages/Pendaftaran/Pendaftaran.jsx
@@ -1,10 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import ReactWhatsapp from "react-whatsapp";
+import "./Pendaftaran.css";
 
 const Pendaftaran = () => {
+  const [values, setValues] = useState({
+    name: "",
+    umur: "",
+    alamat: "",
+    sakit: "",
+    lamasakit: "",
+  });
+  const handleInput = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+    // console.log(values);
+  };
+  const message = JSON.stringify(values);
+
   return (
     <div>
       <Navbar />
+      <form className="pesan">
+        <div className="pesanform">
+          <label htmlFor="name">Nama Pasien</label>
+          <input
+            className="form-control"
+            placeholder="Nama Lengkap"
+            name="name"
+            onChange={handleInput}
+          />
+        </div>
+        <div className="pesanform">
+          <label htmlFor="umur">Umur</label>
+          <input
+            className="form-control"
+            placeholder="Umur"
+            name="umur"
+            onChange={handleInput}
+          />
+        </div>
+        <div className="pesanform">
+          <label htmlFor="alamat">Alamat</label>
+          <input
+            className="form-control"
+            placeholder="Alamat Lengkap"
+            name="alamat"
+            onChange={handleInput}
+          />
+        </div>
+        <div className="pesanform">
+          <label htmlFor="sakit">Sakit</label>
+          <input
+            className="form-control"
+            placeholder="Sakit yang dirasakan"
+            name="sakit"
+            onChange={handleInput}
+          />
+        </div>
+        <div className="pesanform">
+          <label htmlFor="lamasakit">"Lama Sakit</label>
+          <input
+            className="form-control"
+            placeholder="Sudah merasa sakit berapa lama ?"
+            name="lamasakit"
+            onChange={handleInput}
+          />
+        </div>
+        <ReactWhatsapp
+          number="=+6285162665635"
+          message={message}
+          className="tombol-kirim"
+        >
+          Kirim
+        </ReactWhatsapp>
+      </form>
+      <div></div>
     </div>
   );
 };
